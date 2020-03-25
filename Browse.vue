8ad8912c38032bbae6e98cd4ad27127ac5742f56
AxLeExX <59742041+AxLeExX@users.noreply.github.com>
2020-03-24 20:56:25 -0600

    Add files via upload

---
+++ Browse.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="pure-menu pure-menu-horizontal">
+  <ul class="pure-menu-list">
+    <li class="pure-menu-item"><a @click="select('Title')" href="#" class="pure-menu-link">Title</a></li>
+    <li class="pure-menu-item"><a @click="select('Author')" href="#" class="pure-menu-link">Author</a></li>
+    <li class="pure-menu-item"><a @click="select('Rating')" href="#" class="pure-menu-link">Rating</a></li>
+    <li class="pure-menu-item"><a @click="select('ISBN')" href="#" class="pure-menu-link">ISBN</a></li>
+  </ul>
+</div>
+<div class="search">
+  <form class="pure-form">
+    <i class="fas fa-search"></i><input v-model="searchText" />
+  </form>
+</div>
+</template>
+
+
+<script>
+import BookList from "../components/BookList.vue"
+export default {
+  name: 'Browse',
+  components: {
+    BookList
+  },
+  data() {
+    return {
+      searchType: '',
+      searchText: '',
+    }
+  },
+  computed: {
+    books() {
+        if(this.searchType == 'Title') {
+          return this.$root.$data.books.filter(book => book.book_title.toLowerCase().search(this.searchText) >= 0);
+        }
+        else if(this.searchType == 'Author') {
+          return this.$root.$data.books.filter(book => book.author_last_name.toLowerCase().search(this.searchText) && book.author_first_name.toLowerCase().search(this.searchText) >= 0);
+        }
+        //else if(this.searchType == 'Rating') {
+        //  return this.$root.$data.books.filter(book => book.average_rating.search(this.searchText) >= 0);
+        //}
+        else if(this.searchType == 'ISBN') {
+          return this.$root.$data.books.filter(book => book.isbn.search(this.searchText) >= 0);
+        }
+      }
+  },
+  methods: {
+    select(type) {
+      this.searchType = type;
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+</style>
---
+++ Review.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+  <h1>Write a Review</h1>
+  <form v-if="creating" @submit.prevent="addReview">
+    <h2>Book: {{this.book.book_title}}</h2>
+    <h3>Author: {{this.book.author_last_name}}, {{this.book.author_first_name}}</h3>
+    <input v-model="name" placeholder="Name">
+    <p></p>
+    <input v-model="rating" placeholder="Rating">
+    <p></p>
+    <textarea v-model="review" placeholder="Review"></textarea>
+    <br />
+    <button type="submit">Submit</button>
+  </form>
+  <div v-else>
+    <p>Thank you for submitting a review!</p>
+    <p><a @click="toggleForm" href="#">Write another review</a></p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Review',
+  data() {
+    return {
+      creating: true,
+      name: '',
+      rating: '',
+      review: '',
+    }
+  },
+  methods: {
+    toggleForm() {
+      this.creating = !this.creating;
+    },
+    addReview() {
+      this.$root.$data.addReview(this.name, this.rating, this.review);
+      this.name = "";
+      this.rating = "";
+      this.review = "";
+      this.creating = false;
+    },
+  }
+}
+</script>
+
+<style scoped>
+input {
+  font-size: 1.2em;
+  height: 30px;
+  width: 200px;
+}
+
+textarea {
+  font-size: 1.6em;
+  width: 100%;
+  max-width: 500px;
+  height: 100px;
+}
+
+button {
+  margin-top: 20px;
+  font-size: 1.2em;
+}
+</style>
